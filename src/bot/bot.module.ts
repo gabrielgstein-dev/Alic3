3a3ea13ffa02f57503cb5e35933ce0cd1790f82f
Gabriel Stein <gabriel@ritmo.app>
2025-12-29 14:09:19 -0300

    feat: Register DonateCommand as provider in BotModule

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
+import { DonateCommand } from './commands/donate.command';
 import { TicketsModule } from '../tickets/tickets.module';
 import { DiscordModule } from '../discord/discord.module';
 
 @Module({
   imports: [DiscordModule, TicketsModule],
-  providers: [BotService],
+  providers: [BotService, DonateCommand],
 })
 export class BotModule {}
